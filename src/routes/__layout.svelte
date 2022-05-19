ff41076af7a3c4455f90ec5dab3b8cb74d393bb7
Changyuan Lin <coreballium@outlook.com>
2022-05-18 23:00:17 -0400

    Most of the website

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import Header from '$lib/Header.svelte'
+  import '../app.css'
+</script>
+
+<div>
+  <Header />
+
+  <main>
+    <slot />
+  </main>
+
+  <footer>
+    <p>Made in Ithaca, NY</p>
+  </footer>
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    padding: 3rem 2rem;
+    width: fit-content;
+    max-width: 640px;
+    margin: 0 auto;
+  }
+
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
+  footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,75 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module" lang="ts">
+  export const prerender = true
+</script>
+
+<script lang="ts">
+  import GitHubIcon from '$lib/icons/GitHubIcon.svelte'
+  import LinkedInIcon from '$lib/icons/LinkedInIcon.svelte'
+  import MailIcon from '$lib/icons/MailIcon.svelte'
+</script>
+
+<svelte:head>
+  <title>Changyuan Lin</title>
+</svelte:head>
+
+<div class="icons">
+  <a href="https://github.com/Coreball"><GitHubIcon title="Visit GitHub" /></a>
+  <a href="https://www.linkedin.com/in/coreballium/"><LinkedInIcon title="Visit LinkedIn" /></a>
+  <a href="mailto:cl859@cornell.edu"><MailIcon title="Email cl859@cornell.edu" /></a>
+</div>
+
+<p>Hi, I'm Changyuan Lin, a computer science student at Cornell University.</p>
+
+<section>
+  <h2>Currently</h2>
+  <ul>
+    <li>Student at Cornell University</li>
+    <li>
+      Technical Product Manager at <a href="https://www.cornelldti.org/">Cornell DTI</a>
+      <ul>
+        <li>
+          Check out <a href="https://github.com/cornell-dti/zing">Grouper</a> and
+          <a href="https://github.com/cornell-dti/zing-lsc">Zing LSC</a>!
+        </li>
+      </ul>
+    </li>
+  </ul>
+</section>
+
+<section>
+  <h2>Previously</h2>
+  <ul>
+    <li>Software Development Engineer Intern at Amazon</li>
+    <li>Instructor at Coding with Kids</li>
+  </ul>
+</section>
+
+<section>
+  <h2>Links</h2>
+  <ul>
+    <li><a href="https://github.com/Coreball">GitHub</a></li>
+    <li><a href="https://www.linkedin.com/in/coreballium/">LinkedIn</a></li>
+    <li><a href="mailto:cl859@cornell.edu">cl859@cornell.edu</a></li>
+  </ul>
+</section>
+
+<style>
+  section {
+    margin: 0.5rem 0;
+  }
+
+  h2 {
+    font-size: 1rem;
+    margin: 0.5rem 0;
+  }
+
+  ul {
+    margin: 0;
+  }
+
+  .icons {
+    display: flex;
+    gap: 1rem;
+    margin: 1rem 0;
+  }
+</style>
